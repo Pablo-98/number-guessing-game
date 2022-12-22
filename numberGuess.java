4d9334593989f382c5b180faaa28371824b67b8a
Pablo Hernandez <hpablo575@gmail.com>
2022-12-21 18:12:19 -0700

    added scanner

--- numberGuess.java
+++ numberGuess.java
@@ -1,11 +1,28 @@
-import Scanner from java.util.Scanner;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class numberGuess{
     // rules
     //if the guesssed number is bigger than the actual number, the program will respond with the message that the guessed number is higher than the actual number.
 
+    // if the guessed num is smaller than the actual number, the program will respond with the message that the guessed num is lower than the actual num
+    
+    // if the guessed num is equal to the actual num or if the X trials are exhausted the program will end witha suitable message
+
+
 
 public static void main (String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    double random = Math.random();
+    System.out.println(random);
+
+    
+
+   // System.out.println("Enter a number bro");
+  //  int userInput = scan.nextInt();
+
+
 
 }
 }
