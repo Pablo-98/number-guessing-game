87e86e4c17fc775ed94b73cbd82732857f4f13bd
Pablo Hernandez <hpablo575@gmail.com>
2022-12-21 17:51:24 -0700

    first

---
+++ numberGuess.java
@@ -0,0 +1,11 @@
+import Scanner from java.util.Scanner;
+
+public class numberGuess{
+    // rules
+    //if the guesssed number is bigger than the actual number, the program will respond with the message that the guessed number is higher than the actual number.
+
+
+public static void main (String[] args) {
+
+}
+}
